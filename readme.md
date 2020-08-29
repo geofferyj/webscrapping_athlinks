140fe24f065b966e6bca7a960aedf061e7a409d4
Geoffery Joseph <geofferyjoseph1@gmail.com>
2020-08-29 21:00:12 +0100

    added concurrency

--- readme.md
+++ readme.md
@@ -1,3 +1,21 @@
 # Requirements 
 
 chrome 85 installed
+    or 
+chrome 84 installed
+
+# Setup
+
+- Create a virtual environment for example with python-virtualenv
+
+    ` python3 -m venv my_virtual_environment`
+
+- Activate virtual environment 
+    ### in linux 
+    `source my_virtual_environment/bin/activate`
+
+- Install requirements
+
+    `pip install -r requirements.txt`
+
+### Thats it
--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from concurrent.futures.thread import ThreadPoolExecutor
 
 # import other utility modules
 import time, os, re, pandas as pd
@@ -18,13 +19,13 @@ options.add_argument("--start-maximized")
 options.add_argument('window-size=2560,1440')
 
 # open a headless browser with chrome driver in current directory
-browser1 = webdriver.Chrome("./chromedriver", options=options)
+browser = webdriver.Chrome("./chromedriver84", options=options)
 
 # set delay on browser to allow page load data
-browser1.implicitly_wait(10)
+browser.implicitly_wait(10)
 
 # make request to the results page
-browser1.get("https://www.athlinks.com/event/18578/results/Event/901227/Course/1763149/Results")
+browser.get("https://www.athlinks.com/event/18578/results/Event/901227/Course/1763149/Results")
 
 # store page source in this variable
 source = ""
@@ -35,7 +36,7 @@ while True:
     # pause for additional 5 seconds to make sure page has loaded completely
     time.sleep(5)
     
-    source += browser1.page_source
+    source += browser.page_source
     print(f"copied page {x}")
     
     # try to find next button 
@@ -43,17 +44,17 @@ while True:
     try:
         
         # find the button
-        next_button = browser1.find_element_by_xpath('//button[text()=">"]')
+        next_button = browser.find_element_by_xpath('//button[text()=">"]')
         
         # click it
-        browser1.execute_script("arguments[0].click();", next_button)
+        browser.execute_script("arguments[0].click();", next_button)
     except NoSuchElementException:
         # if not found (i.e at the last page)
         # break out of (exit) the loop
         break
 
 # close the browser
-browser1.quit()
+browser.quit()
 
 # find and extract all bibs from page source
 bibs = re.findall("(?<=/event/18578/results/Event/901227/Course/1763149/Bib/)\d+", source)
@@ -63,11 +64,30 @@ chunks = [bibs[x:x+50] for x in range(0, len(bibs), 50)]
 
 
 # open another browser
+browser1 = webdriver.Chrome("./chromedriver", options=options)
 browser2 = webdriver.Chrome("./chromedriver", options=options)
-browser2.implicitly_wait(6)
+browser3 = webdriver.Chrome("./chromedriver", options=options)
+browser4 = webdriver.Chrome("./chromedriver", options=options)
+browser5 = webdriver.Chrome("./chromedriver", options=options)
+browser6 = webdriver.Chrome("./chromedriver", options=options)
+browser7 = webdriver.Chrome("./chromedriver", options=options)
+browser8 = webdriver.Chrome("./chromedriver", options=options)
+browser9 = webdriver.Chrome("./chromedriver", options=options)
+browser10 = webdriver.Chrome("./chromedriver", options=options)
+browser11 = webdriver.Chrome("./chromedriver", options=options)
+browser12 = webdriver.Chrome("./chromedriver", options=options)
 
-# dictionary to hold data for each participant
-results_dict = {"Bib":[],
+browsers = [browser1,browser2,browser3,browser4,browser5,browser6,browser7,browser8,browser9,browser10,browser11,browser12]
+
+print(len(browsers), len(chunks), len(range(len(browsers))))
+
+
+# iterate over the list of bibs and make requests
+
+def thread_func(browser, x, chunk):
+    print(f"---------------------------- BEGIN PROCESSING FOR CHUNK {x} ----------------------------")
+    # dictionary to hold data for each participant
+    results_dict = {"Bib":[],
                "Name":[],
                "Gender":[],
                "City/State":[],
@@ -77,42 +97,40 @@ results_dict = {"Bib":[],
                "Half Time":[],
                "30K Time":[],
                "Full Course Time":[]}
-
-# iterate over the list of bibs and make requests
-x = 0
-for chunk in chunks:
-    x += 1
+    
+    browser.implicitly_wait(6)
+    
     for bib in chunk:
         
-        browser2.get(f"https://www.athlinks.com/event/18578/results/Event/901227/Course/1763149/Bib/{bib}")
+        browser.get(f"https://www.athlinks.com/event/18578/results/Event/901227/Course/1763149/Bib/{bib}")
         
-       
+    
         # select the name of participant from page
-        name = browser2.find_element_by_id("athlete-profile-link")
+        name = browser.find_element_by_id("athlete-profile-link")
         
         # select the gender of participant from page
-        gender = browser2.find_element_by_id("ageGender")
+        gender = browser.find_element_by_id("ageGender")
         
         # select the city/state of participant from page
-        city_state = browser2.find_element_by_id("IRPUserLocation")
+        city_state = browser.find_element_by_id("IRPUserLocation")
         
         # select the chip start time of participant from page
-        chip_start_time = browser2.find_element_by_xpath('//div[text()="Chip Start Time"]/following-sibling::div[1]')
+        chip_start_time = browser.find_element_by_xpath('//div[text()="Chip Start Time"]/following-sibling::div[1]')
         
         # select the gun time of participant from page
-        gun_time = browser2.find_element_by_xpath('//div[text()="Gun time"]/following-sibling::div[1]')
+        gun_time = browser.find_element_by_xpath('//div[text()="Gun time"]/following-sibling::div[1]')
         
         # select the 10K time of participant from page
-        _10k = browser2.find_element_by_xpath('//div[text()="10K"]/following-sibling::div[last()]')
+        _10k = browser.find_element_by_xpath('//div[text()="10K"]/following-sibling::div[last()]')
         
         # select the half time of participant from page
-        half = browser2.find_element_by_xpath('//div[text()="HALF"]/following-sibling::div[last()]')
+        half = browser.find_element_by_xpath('//div[text()="HALF"]/following-sibling::div[last()]')
         
         # select the 30K time of participant from page
-        _30k = browser2.find_element_by_xpath('//div[text()="30K"]/following-sibling::div[last()]')
+        _30k = browser.find_element_by_xpath('//div[text()="30K"]/following-sibling::div[last()]')
         
         # select the full course time of participant from page
-        full_course = browser2.find_element_by_xpath('//div[text()="Full Course"]/following-sibling::div[last()]')            
+        full_course = browser.find_element_by_xpath('//div[text()="Full Course"]/following-sibling::div[last()]')            
         
     
         # -------- create entry for participant in results_dict
@@ -128,10 +146,10 @@ for chunk in chunks:
         results_dict["Full Course Time"].append(full_course.text)
         
         
-        print(f"---> Entry for {name.text} created")
+        print(f"---> Entry for {name.text} created in chuck {x}")
 
     # close second browser
-    browser2.quit()
+    browser.quit()
 
     # convert results dict to a panda dataframe
     df = pd.DataFrame(results_dict)
@@ -141,6 +159,28 @@ for chunk in chunks:
 
     print(f"---------------------------- END OF PROCESSING FOR CHUNK {x} ----------------------------")
     
+#  Added concurrency for performance boost
+with ThreadPoolExecutor() as executor:
+    
+    [executor.submit(thread_func, b,x, chunk) for (b, x, chunk) in zip(browsers[0:3], range(len(browsers))[0:3], chunks[0:3])]
+
+
+with ThreadPoolExecutor() as executor:
+    
+    [executor.submit(thread_func, b,x, chunk) for (b, x, chunk) in zip(browsers[3:6], range(len(browsers))[3:6], chunks[3:6])]
+
+
+with ThreadPoolExecutor() as executor:
+    
+    [executor.submit(thread_func, b,x, chunk) for (b, x, chunk) in zip(browsers[6:9], range(len(browsers))[6:9], chunks[6:9])]
+
+
+with ThreadPoolExecutor() as executor:
+    
+    executor.submit(thread_func, browsers[11], range(len(browsers))[11], chunks[11])
+
+
+# concatenate all chucks into one file
 df_list = os.listdir("result")
-result_frame = pd.concat((pd.read_csv(f) for f in df_list))
-result_frame.to_csv("data.csv")
+result_frame = pd.concat((pd.read_csv(f"result/{f}") for f in df_list))
+result_frame.to_csv("data.csv", index=False)
